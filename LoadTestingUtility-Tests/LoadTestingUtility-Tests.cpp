75189914e3c7f78524b0412b7cd4fa7d5bcf4495
dmitrii-zagornyi <dmitry.zagorny@gmail.com>
2021-08-04 03:21:05 +0300

    Initial commit

---
+++ LoadTestingUtility-Tests/LoadTestingUtility-Tests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LoadTestingUtility/LoadTestingUtility.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LoadTestingUtilityTests
+{
+	TEST_CLASS(LoadTestingUtilityTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
