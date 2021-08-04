e2e290d1d3ebccd3e39360373073769bf666f80c
dmitrii-zagornyi <dmitry.zagorny@gmail.com>
2021-08-04 07:26:27 +0300

    Created functions for send get requests

--- LoadTestingUtility-Tests/LoadTestingUtility-Tests.cpp
+++ LoadTestingUtility-Tests/LoadTestingUtility-Tests.cpp
@@ -1,17 +1,186 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+
 #include "../LoadTestingUtility/LoadTestingUtility.cpp"
 
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
 
 namespace LoadTestingUtilityTests
 {
 	TEST_CLASS(LoadTestingUtilityTests)
 	{
+	private:
+		string urlValid = "https://www.sberbank.ru";
+		string urlInvalid = "https://www.google.com1234";
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CheckStatus_Ok)
 		{
+			::CheckStatus(CURLE_OK);
+		}
+		TEST_METHOD(CheckStatus_Fail)
+		{
+			auto func = [this] { ::CheckStatus(CURLE_UNSUPPORTED_PROTOCOL); };
+			Assert::ExpectException<std::exception>(func);
+		}
+		TEST_METHOD(SendGetRequest_Default)
+		{
+			CURL* curl = curl_easy_init();
+
+			map<long, vector<double>> response;
+
+			SendGetRequest(curl, urlValid, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == 1);
+			Assert::IsTrue(response[response.begin()->first][0] < 2.0);
+
+			curl_easy_cleanup(curl);
+		}
+		TEST_METHOD(SendGetRequest_NotEmptyMapAnotherResponse)
+		{
+			CURL* curl = curl_easy_init();
+
+			map<long, vector<double>> response;
+
+			response[2001].push_back(5.0);
+
+			SendGetRequest(curl, urlValid, response);
+
+			Assert::IsTrue(response.size() == 2);
+			Assert::IsTrue(response[response.begin()->first].size() == 1);
+			Assert::IsTrue(response[response.begin()->first][0] < 2.0);
+			Assert::IsTrue(response[2001].size() == 1);
+			Assert::IsTrue(response[2001][0] == 5.0);
+
+			curl_easy_cleanup(curl);
+		}
+		TEST_METHOD(SendGetRequest_IncorrectUrl)
+		{
+			auto func = [this] {
+				CURL* curl = curl_easy_init();
+				map<long, vector<double>> response;
+				SendGetRequest(curl, urlInvalid, response); };
+			Assert::ExpectException<std::exception>(func);
+		}
+		TEST_METHOD(SendGetRequest_CurlNotInit)
+		{
+			auto func = [this] {
+				map<long, vector<double>> response;
+				SendGetRequest(NULL, urlValid, response); };
+			Assert::ExpectException<std::exception>(func);
+		}
+		TEST_METHOD(SendBatchGetRequest_Default)
+		{
+			constexpr unsigned int requestsCount = 3;
+
+			map<long, vector<double>> response;
+
+			SendBatchGetRequest(urlValid, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == requestsCount);
+			for (unsigned int i = 0; i < requestsCount; i++)
+			{
+				Assert::IsTrue(response[response.begin()->first][i] < 2.0);
+			}
+		}
+		TEST_METHOD(SendBatchRequest_Zero)
+		{
+			constexpr unsigned int requestsCount = 0;
+
+			map<long, vector<double>> response;
+
+			SendBatchGetRequest(urlValid, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 0);
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_ZeroThreadZeroRequests)
+		{
+			constexpr unsigned int threadsCount = 0;
+			constexpr unsigned int requestsCount = 0;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 0);
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_OneThreadZeroRequests)
+		{
+			constexpr unsigned int threadsCount = 1;
+			constexpr unsigned int requestsCount = 0;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 0);
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_OneThreadOneRequest)
+		{
+			constexpr unsigned int threadsCount = 1;
+			constexpr unsigned int requestsCount = 1;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == threadsCount * requestsCount);
+			Assert::IsTrue(response[response.begin()->first][0] < 2.0);
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_OneThreadManyRequest)
+		{
+			constexpr unsigned int threadsCount = 1;
+			constexpr unsigned int requestsCount = 3;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == threadsCount * requestsCount);
+
+			for (unsigned int i = 0; i < requestsCount; i++)
+			{
+				Assert::IsTrue(response[response.begin()->first][i] < 2.0);
+			}
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_ManyThreadOneRequest)
+		{
+			constexpr unsigned int threadsCount = 3;
+			constexpr unsigned int requestsCount = 1;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == threadsCount * requestsCount);
+
+			for (unsigned int i = 0; i < requestsCount; i++)
+			{
+				Assert::IsTrue(response[response.begin()->first][i] < 2.0);
+			}
+		}
+		TEST_METHOD(SendParallelBatchGetRequest_ManyThreadManyRequest)
+		{
+			constexpr unsigned int threadsCount = 3;
+			constexpr unsigned int requestsCount = 2;
+
+			map<long, vector<double>> response;
+
+			SendParallelBatchGetRequest(urlValid, threadsCount, requestsCount, response);
+
+			Assert::IsTrue(response.size() == 1);
+			Assert::IsTrue(response[response.begin()->first].size() == threadsCount * requestsCount);
+
+			for (unsigned int i = 0; i < requestsCount; i++)
+			{
+				Assert::IsTrue(response[response.begin()->first][i] < 2.0);
+			}
 		}
 	};
 }
--- LoadTestingUtility/LoadTestingUtility.cpp
+++ LoadTestingUtility/LoadTestingUtility.cpp
@@ -1,34 +1,99 @@
-// LoadTestingUtility.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <functional>
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
 
 #define CURL_STATICLIB
 #include <curl\curl.h>
 
-int main()
+
+using namespace std;
+
+
+void CheckStatus(CURLcode hr)
 {
-    CURL* curl;
+    if (hr != CURLE_OK)
+    {
+        throw runtime_error(curl_easy_strerror(hr));
+    }
+}
 
-    curl = curl_easy_init();
+void SendGetRequest(CURL* curl, string url, map<long, vector<double>>& response)
+{
+    CURLcode hr;
+
+    hr = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    CheckStatus(hr);
+
+    hr = curl_easy_perform(curl);
+    CheckStatus(hr);
+
+    long response_code;
+    double elapsed;
+    hr = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    CheckStatus(hr);
+
+    hr = curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
+    CheckStatus(hr);
+
+    response[response_code].push_back(elapsed);
+}
+
+void SendBatchGetRequest(string url, unsigned int requestsCount, map<long, vector<double>>& response)
+{
+    CURL* curl = curl_easy_init();
+
+    for (unsigned int i = 0; i < requestsCount; i++)
+    {
+        SendGetRequest(curl, url, response);
+    }
+
     curl_easy_cleanup(curl);
+}
 
-    return 0;
+void SendParallelBatchGetRequest(string url, unsigned int threadsCount, unsigned int requestsCount, map<long, vector<double>>& response)
+{
+    vector<pair<thread*, map<long, vector<double>>*>> batchGetRequests;
+
+    for (unsigned int i = 0; i < threadsCount; i++)
+    {
+        map<long, vector<double>>* pThreadResponse = new map<long, vector<double>>;
+        thread* pThread = new std::thread(&SendBatchGetRequest, url, requestsCount, ref(response));
+        batchGetRequests.push_back(make_pair(pThread, pThreadResponse));
+    }
+
+    for (unsigned int i = 0; i < threadsCount; i++)
+    {
+        batchGetRequests[i].first->join();
+
+        map<long, vector<double>>* threadResponse = batchGetRequests[i].second;
+        for (auto it = threadResponse->begin(); it != threadResponse->end(); it++)
+        {
+            for (unsigned int j = 0; j < it->second.size(); j++)
+            {
+                response[it->first].push_back(it->second[j]);
+            }
+        }
+    }
+
+    for (unsigned int i = 0; i < threadsCount; i++)
+    {
+        delete batchGetRequests[i].first;
+        delete batchGetRequests[i].second;
+    }
 }
 
 
-//int main()
-//{
-//    std::cout << "Hello World!\n";
-//}
+int main()
+{
+    constexpr unsigned int threadsCount = 2;
+    constexpr unsigned int requestsCount = 100;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    map<long, vector<double>> response;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    SendParallelBatchGetRequest("https://habr.com/ru/all/", threadsCount, requestsCount, response);
+
+    return 0;
+}
